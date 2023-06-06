d13646cc2fcb3c1a062bc46e02bc0c727d4ce665
Joe de Casanove <jlvdecasanove@gmail.com>
2023-06-06 23:08:34 +0100

    added get and post requests

--- server.js
+++ server.js
@@ -8,3 +8,34 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+  (`Server is listening on port ${PORT}`)
+});
+
+app.get('/api/quotes/random', (req, res, next) => {
+    randomQuote = getRandomElement(quotes);
+    res.send({quote: randomQuote});
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const personQuery = req.query;
+    if (!personQuery.person) {
+        res.send({quotes: quotes});
+    } else {
+        const personQuotes = quotes.filter(quote => quote.person === personQuery.person);
+        res.send({quotes: personQuotes});
+    } 
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    const newQuote = {
+        quote: req.query.quote,
+        person: req.query.person
+    };
+    if (newQuote.quote && newQuote.person) {
+        quotes.push(newQuote);
+        res.send({quote: newQuote});
+    } else {
+        res.status(400).send();
+    }
+});
